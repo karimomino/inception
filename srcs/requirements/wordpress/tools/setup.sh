a357011aab8c6c78bd11fe6f85adc81230084e16
Karim Amin <kamin@lab2r4s8.42abudhabi.ae>
2023-11-10 18:35:42 +0400

    Modified the while loop in wordpress setup script

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -2,11 +2,11 @@
 
 if [ ! -f "/var/www/wordpress/wp-config.php" ]
 then
-    while [ wp config create    --allow-root \
+    while !wp config create    --allow-root \
                         --dbname=$MYSQL_DATABASE \
                         --dbuser=$MYSQL_USER \
                         --dbpass=$MYSQL_PASSWORD \
-                        --dbhost=mariadb:3306 --path='/var/www/wordpress' ]
+                        --dbhost=mariadb:3306 --path='/var/www/wordpress';
     do
     sleep 2
     done
